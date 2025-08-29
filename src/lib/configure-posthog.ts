03af5d4db60218116aa8ff0e33d64eb08ea58ef9
Julian Curalli <julian.curalli27@gmail.com>
2025-08-28 23:20:14 -0400

    more posthog changes

--- src/lib/configure-posthog.ts
+++ src/lib/configure-posthog.ts
@@ -1,15 +1,19 @@
 import type { OpenAPIHono } from "@hono/zod-openapi";
-import { env } from "hono/adapter";
+import { env as honoEnv } from "hono/adapter";
 import { createMiddleware } from "hono/factory";
 import { PostHog } from "posthog-node";
+import env from "../env.js";
 
 const posthogMiddleware = createMiddleware(async (c, next) => {
-  const { POSTHOG_PUBLIC_KEY } = env<{ POSTHOG_PUBLIC_KEY: string }>(c);
+  const { POSTHOG_PUBLIC_KEY } = honoEnv<{ POSTHOG_PUBLIC_KEY: string }>(c);
   const posthog = new PostHog(POSTHOG_PUBLIC_KEY, { host: "https://us.i.posthog.com" });
 
   posthog.capture({
     distinctId: c.req.header("x-rhr-scouting-api-key") ?? "unknown",
-    event: `${c.req.method} ${c.req.url}`,
+    event: `${c.req.method} ${c.req.path}`,
+    properties: {
+      environment: env.NODE_ENV,
+    },
   });
 
   await posthog.shutdown();
